e960e7de14b5e67cb740a794f425a2bb84db88f3
Xavier Julián <xavier.julian@kaleidos.net>
2013-12-17 19:27:02 +0100

    Adding Jade basic compiling and example template

--- hythloday-front/app/scripts/controllers/main.js
+++ hythloday-front/app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('hythlodayApp')
-  .controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
     ];
-  });
+});
