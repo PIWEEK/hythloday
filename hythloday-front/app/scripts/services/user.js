904beeecf75f14b1da63434b5e16a4cee3d245fc
Xavier Julián <xavier.julian@kaleidos.net>
2013-12-19 19:01:02 +0100

    Small fixes in user data

--- hythloday-front/app/scripts/services/user.js
+++ hythloday-front/app/scripts/services/user.js
@@ -5,7 +5,7 @@ angular.module('hythlodayApp')
     .factory('User', function () {
         var user = {};
 
-        user.id = 7;
+        user.id = 16;
         user.avatar = 'http://localhost:8080/web-images/avatar/user_avatar10.png';
         user.name = 'Hannah Montana';
 
